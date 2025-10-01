4932b2606f243b4c422c2020ccdab8965308d347
yunmin12 <waterduck1020@gmail.com>
2025-10-01 19:58:00 +0900

    Feat: Bash script for score_vis.py

---
+++ sh/score_vis.sh
@@ -0,0 +1,5 @@
+python -u /home/yunmin/proj/LigandMPNN/scripts/score_vis.py \
+--results_dir ~/proj/data/bench/outputs/1001/scores \
+--out_dir ~/proj/data/bench/outputs/1001/scores/aldo_keto \
+--mutation 'A:298:C>A' --mutation 'A:219:W>Y' \
+--pdb_ids 2IPW,2IS7,2INZ,2INE
