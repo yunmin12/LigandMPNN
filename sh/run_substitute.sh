9c60609753664762c97801403dc72ca375a72747
yunmin12 <waterduck1020@gmail.com>
2025-10-01 19:57:18 +0900

    Feat: Bash script for ligand substitution before RFDAA

--- sh/run_substitute.sh
+++ sh/run_substitute.sh
@@ -4,8 +4,8 @@ old_resname=$3
 old_chain=$4
 old_resid=$5
 python /home/yunmin/proj/LigandMPNN/scripts/substitute.py \
---complex_pdb ${protein} \
---new_ligand ${ligand} \
+--complex_pdb /home/yunmin/proj/data/db/bench/${protein}.pdb \
+--new_ligand /home/yunmin/proj/data/db/bench/${ligand}.mol2 \
 --old_resname ${old_resname} \
 --old_chain ${old_chain} \
 --old_resid ${old_resid} \
