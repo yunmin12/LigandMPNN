5e71cc87027c0481393280d01f3fa6b46d441657
yunmin12 <waterduck1020@gmail.com>
2025-10-23 11:09:51 +0900

    Feat: bash script for bindingdb_v2.py

---
+++ sh/run_bdb_v2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -J bdb
+#SBATCH -c 4
+#SBATCH -q normal
+#SBATCH -p cpu
+#SBATCH --mem=16G
+
+# source /home/yunmin/miniforge3/bin/activate ligandmpnn_env
+
+source /apps/.slurmrc
+slurm_start $SLURM_CHANNEL_ID
+###############################################
+python /home/yunmin/proj/LigandMPNN/dataprep/bindingdb_v2.py \
+  --out /home/yunmin/proj/data/db/15_eval_set_1015_ensure_pdb_same_mut.csv \
+  --input articles --ensure_pdb True --same_mutation True
+###############################################
+slurm_end $SLURM_CHANNEL_ID
