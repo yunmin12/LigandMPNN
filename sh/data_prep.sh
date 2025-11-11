6b86291a34b705033230695d45dbbed6b77092d1
yunmin12 <waterduck1020@gmail.com>
2025-11-11 15:46:53 +0900

    Fix: fixed paths and added ensemble filtering to the pipeline

--- sh/data_prep.sh
+++ sh/data_prep.sh
@@ -1,4 +1,5 @@
 $sh_dir="/home/yunmin/proj/LigandMPNN/sh"
+$base_dir="/home/yunmin/proj/data/db/v2"
 
 # 1. Clean target and off-target complex PDB
 bash $sh_dir/clean_hetatm.sh tar
@@ -16,12 +17,25 @@ bash $sh_dir/split.sh tar
 bash $sh_dir/split.sh off
 echo "✅ [split] done."
 
-python /home/yunmin/proj/LigandMPNN/scripts/recover_v2.py \
+python /home/yunmin/proj/LigandMPNN/dataprep/recover_v2.py \
     $base_dir \
     --mode tar
 echo "✅ [recover] target done."
 
-python /home/yunmin/proj/LigandMPNN/scripts/recover_v2.py \
+python /home/yunmin/proj/LigandMPNN/dataprep/recover_v2.py \
     $base_dir \
     --mode off
 echo "✅ [recover] off-target done."
+
+# 4. Filter PLACER output ensemble
+python /home/yunmin/proj/LigandMPNN/dataprep/ens_filtering.py \
+    --base_dir $base_dir \
+    --type tar \
+    --mode self
+echo "✅ [filter] target done."
+python /home/yunmin/proj/LigandMPNN/dataprep/ens_filtering.py \
+    --base_dir $base_dir \
+    --type off \
+    --mode cross
+echo "✅ [filter] off-target done."
+
