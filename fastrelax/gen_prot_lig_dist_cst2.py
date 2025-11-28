4f475c3f27997db28e41dcdc7892045cd68a509a
yunmin12 <waterduck1020@gmail.com>
2025-11-28 14:43:55 +0900

    Fix: FastRelax no repack, no h bond version

--- fastrelax/gen_prot_lig_dist_cst2.py
+++ fastrelax/gen_prot_lig_dist_cst2.py
@@ -2,110 +2,102 @@ import os
 import sys
 import numpy as np
 import math
-#from pathlib import Path
-#script_path = str(Path(__file__).resolve().parent)
-#sys.path.insert(0,script_path+'/../_lib')
-from libPDB import Atom
-from libProtein import three_aa_to_one_aa
 from collections import defaultdict
 
-class Atom:
-    def __init__(self,line):
-        self.raw_line = line
-        self.line = line.strip()
-        self.chainID = line[21]
-        self.atmNo = int(line[7:11])
-        self.atmName = line[11:17].strip()
-        self.resNo = int(line[22:26])
-        self.alt_state = line[16]
-        self.resName = line[17:21].strip()
-        self.oneLetter = three_aa_to_one_aa(self.resName)
-        self.coord = [float(line[30:38]),\
-                      float(line[38:46]),\
-                      float(line[46:54])]
-        #
-        self.rep_resNo = '%s%d'%(self.chainID,self.resNo)
-        self.template = '%s'%(line[:30])
-        self.full_template = line.strip()
-        return
+import pyrosetta
 
-def dist_two_coords(crd_a,crd_b):
-    a = np.array(crd_a)
-    b = np.array(crd_b)
-    d_xyz = a-b
-    d2_sum = []
-    for d in d_xyz:
-        d2_sum.append(d*d)
-    return math.sqrt(sum(d2_sum))
-
-
-#example
-#'AtomPair NE1 11 O 7 HARMONIC 3.0 0.5'
+# example: 'AtomPair NE1 11 O 7 HARMONIC 3.0 0.5'
 CST_STDERR = 0.5
 
-#Assume there is one type of hetatm ligand and that is our target
-def extract_dist_cst_from_pdb_use_allatm(pdb_in,ptm_resname,bsite_res=''):
-    #
-    #use all_res (only chainA that is not a peptide)
-    if bsite_res == '':
-        bsite_res_s = []
-        with open(pdb_in) as fp:
-            for line in fp:
-                if line.startswith('ATOM') and line[13:15] == 'CA':
-                    atm = Atom(line.strip())
-                    if atm.chainID == 'A':
-                        bsite_res_s.append(int(line[22:26]))
+
+def _pose_index_from_pdb_info(pdb_info, chain, resno):
+    """Map a PDB chain/resno to the corresponding pose index."""
+    for i in range(1, pdb_info.nres() + 1):
+        if pdb_info.chain(i) == chain and pdb_info.number(i) == resno:
+            return i
+    raise ValueError(f"Could not find pose index for {chain}{resno}")
+
+
+def extract_dist_cst_from_pdb_use_allatm(
+    pdb_in, ptm_resname, bsite_res="", prot_chains=("A", "B"), lig_chain=None
+):
+    """
+    Build AtomPair constraints using pose indices (not raw PDB numbering).
+
+    - Uses all CA atoms from protein chains in `prot_chains` (skip ligand chain).
+    - Identifies the first residue with name `ptm_resname` as the ligand
+      (optionally restricted to `lig_chain`).
+    - For each ligand atom, finds the closest protein CA and writes an AtomPair
+      line with pose indices.
+    """
+    pose = pyrosetta.pose_from_pdb(pdb_in)
+    pdb_info = pose.pdb_info()
+
+    # Identify ligand residue (pose index) and collect its atom names/coords.
+    lig_pose_idx = None
+    lig_atom_names = []
+    lig_atom_coords = {}
+    for i in range(1, pose.size() + 1):
+        if pose.residue(i).name3().strip() != ptm_resname:
+            continue
+        if lig_chain and pdb_info.chain(i) != lig_chain:
+            continue
+        lig_pose_idx = i
+        res = pose.residue(i)
+        for atm_i in range(1, res.natoms() + 1):
+            atm_name = res.atom_name(atm_i).strip()
+            lig_atom_names.append(atm_name)
+            lig_atom_coords[atm_name] = res.xyz(atm_i)
+        break
+    if lig_pose_idx is None:
+        raise ValueError(f"Ligand {ptm_resname} not found in {pdb_in}")
+
+    lig_chain = pdb_info.chain(lig_pose_idx)
+
+    # Collect protein CA coords (pose indices) for the requested chains.
+    bsite_pose_indices = []
+    if bsite_res:
+        # If provided, interpret as comma-separated PDB residue numbers on prot_chains[0].
+        resnos = [int(x) for x in bsite_res.split(",") if x]
+        for r in resnos:
+            try:
+                idx = _pose_index_from_pdb_info(pdb_info, prot_chains[0], r)
+                bsite_pose_indices.append(idx)
+            except ValueError:
+                print(f"[WARN] Skipping missing residue {prot_chains[0]}{r} in {pdb_in}")
     else:
-        bsite_res_s = [int(x) for x in bsite_res.split(',')]                    
-        
-    #Will get distances between each ligand target atom and the closest binding site residue CA
-    #First read ligand target atom and binding site residue CA coords
-    bsite_CA_R = {}
-    het_atm_R = {}
-    het_resno = None
-    lig_tr_atms = []
-    #in case the ligand residue is not numbered sequentially (e.g. diffusion output)
-    last_prot_resno = 0
-    with open(pdb_in) as fp:
-        for line in fp:
-            if line.startswith('ATOM'):
-                atm = Atom(line.strip())
-                last_prot_resno = atm.resNo
-                if atm.chainID == 'A' and atm.resNo in bsite_res_s and atm.atmName == 'CA':
-                    bsite_CA_R[atm.resNo] = atm.coord
-            elif line.startswith('HETATM'):
-                atm = Atom(line.strip())
-                if atm.resName == ptm_resname:
-                    het_resno = last_prot_resno + 1
-                    lig_tr_atms.append(atm.atmName)
-                    het_atm_R[atm.atmName] = atm.coord
-    #
-    #in case the ligand residue is not numbered sequentially (e.g. diffusion output)
-    #het_resno = last_prot_resno + 1
-    #for each ligand target atm get distances from each bsite residue CA and sort
-    cst_s = []
-    for hetatm in lig_tr_atms:
-        het_R = het_atm_R[hetatm]
-        #
-        het_CA_d = {}
-        for resno in bsite_res_s:
-            if resno not in bsite_CA_R:
-                print(f"[WARN] Skipping resno {resno} which is not in bsite_CA_R")
+        for i in range(1, pose.size() + 1):
+            if i == lig_pose_idx:
                 continue
-            CA_R = bsite_CA_R[resno]
-            d = dist_two_coords(het_R,CA_R)
-            het_CA_d[resno] = d
-        #
-        sorted_ds = sorted(het_CA_d.items(), key=lambda item: item[1])
-        closest = sorted_ds[0]
-        closest_resno = closest[0]
-        closest_d = closest[1]
-        #
-        
-        cst_line = f'AtomPair {hetatm} {het_resno} CA {closest_resno} HARMONIC {closest_d} {CST_STDERR}'
+            ch = pdb_info.chain(i)
+            if ch == lig_chain:
+                continue
+            if ch not in prot_chains:
+                continue
+            res = pose.residue(i)
+            if res.has("CA"):
+                bsite_pose_indices.append(i)
+
+    if not bsite_pose_indices:
+        raise ValueError(f"No binding-site CA residues found in chains {prot_chains} for {pdb_in}")
+
+    # Build constraints: for each ligand atom, find nearest protein CA.
+    cst_s = []
+    for lig_atm in lig_atom_names:
+        lig_coord = lig_atom_coords[lig_atm]
+        het_CA_d = {}
+        for idx in bsite_pose_indices:
+            res = pose.residue(idx)
+            if not res.has("CA"):
+                continue
+            ca_coord = res.xyz("CA")
+            d = (lig_coord - ca_coord).norm()
+            het_CA_d[idx] = d
+        if not het_CA_d:
+            continue
+        closest_idx, closest_d = sorted(het_CA_d.items(), key=lambda item: item[1])[0]
+        cst_line = f"AtomPair {lig_atm} {lig_pose_idx} CA {closest_idx} HARMONIC {closest_d} {CST_STDERR}"
         cst_s.append(cst_line)
+
     return cst_s
-        
-                
-                    
     
--- fastrelax/relax_sc_simple_hb_offrepack_edit.py
+++ fastrelax/relax_sc_simple_hb_offrepack_edit.py
@@ -8,11 +8,8 @@ import pandas as pd
 import pyrosetta
 from pyrosetta.rosetta.protocols.simple_moves import *
 import pyrosetta.rosetta.protocols.rosetta_scripts as rosetta_scripts
-
-import pyrosetta.distributed.io as io
-import pyrosetta.distributed.packed_pose as packed_pose
-import pyrosetta.distributed.tasks.rosetta_scripts as rosetta_scripts
-import pyrosetta.distributed.tasks.score as score
+import tempfile
+import os
 
 # from xml_relax_after_ligMPNN_PTM import XML_BSITE_REPACK_MIN_BETA
 from xml_relax_after_ligMPNN_PTM_offrepack import XML_BSITE_REPACK_MIN_BETA
@@ -20,26 +17,49 @@ from gen_prot_lig_dist_cst2 import extract_dist_cst_from_pdb_use_allatm,CST_STDE
 from get_pock_res_by_dist_lig import PocketPDB
 
 def repack_pose(work_pose, nres, packable_res, xml_in, tag, cst_fn):
-    #cst_fn is global var.
-    repack_protocol = xml_in.format(packable_res,cst_fn)
-#    repack_protocol = xml_in.format(packable_res,nres)
-#    pyrosetta.rosetta.core.pose.setPoseExtraScore(work_pose,"tag",'%s'%tag)
-    return rosetta_scripts.SingleoutputRosettaScriptsTask(repack_protocol), work_pose
+    """Run the RosettaScripts XML and return the modified pose and filter scores."""
+    repack_protocol = xml_in.format(packable_res, cst_fn)
+    # Build mover from XML and apply in-place to the pose
+    parser = rosetta_scripts.RosettaScriptsParser()
+    with tempfile.NamedTemporaryFile("w", suffix=".xml", delete=False) as tmp_xml:
+        tmp_xml.write(repack_protocol)
+        tmp_xml_path = tmp_xml.name
+    try:
+        protocol_mover = parser.generate_mover(tmp_xml_path)
+        protocol_mover.apply(work_pose)
+    finally:
+        try:
+            os.remove(tmp_xml_path)
+        except OSError:
+            pass
+
+    # Evaluate filters explicitly to collect scores
+    xml_obj = rosetta_scripts.XmlObjects.create_from_string(repack_protocol)
+    filter_names = ["fa_rep", "ddg", "cms", "res_totalscore", "totalscore"]
+    scores = {}
+    for fname in filter_names:
+        try:
+            fobj = xml_obj.get_filter(fname)
+            scores[fname] = fobj.report_sm(work_pose)
+        except Exception as exc:
+            print(f"[WARN] Failed to evaluate filter {fname}: {exc}")
+    return work_pose, scores
 
 class RelaxScore:
-    def __init__(self,pdbfn,target_hb_atms,repack_res=[],param_fn=None):
+    def __init__(self,pdbfn,target_hb_atms,repack_res=[],param_fn=None,seed=12345):
         self.pdbfn = pdbfn.strip()
         self.target_hb_atms = target_hb_atms.split(',')
         self.param_fn = param_fn
         self.tag = self.pdbfn.split('/')[-1].split('.pdb')[0]
         self.output_dir = '/'.join(self.pdbfn.split('/')[:-1])
-        self.cst_fn = f'{self.output_dir}/{self.tag}.cst'
+        self.seed = seed
+        self.cst_fn = f'{self.output_dir}/{self.tag}_{self.seed}.cst'
         self.repack_res = repack_res
         self.repack_all = False
         if (len(self.repack_res) == 0):
             self.repack_all = True
     def bsite_repack_min(self,pose_work):
-        self.nres = len(pose_work)
+        self.nres = pose_work.total_residue()
         #Repack all if not any residue is assigned
         if self.repack_all and len(self.repack_res) == 0:
             self.repack_res = []
@@ -47,14 +67,19 @@ class RelaxScore:
                 self.repack_res.append(ires)
             #
         repackable_res_str = ','.join(['%d'%resno for resno in self.repack_res])
-        xml_obj, pose_work = repack_pose(pose_work,self.nres,repackable_res_str,XML_BSITE_REPACK_MIN_BETA,self.tag,self.cst_fn)
-        xml_obj.setup()
-        pose_work = xml_obj(pose_work)
-        out_pdb = '%s/%s_betarelax.pdb'%(self.output_dir,self.tag)
-        return pose_work, out_pdb
+        pose_work, filter_scores = repack_pose(
+            pose_work,
+            self.nres,
+            repackable_res_str,
+            XML_BSITE_REPACK_MIN_BETA,
+            self.tag,
+            self.cst_fn,
+        )
+        out_pdb = f'{self.output_dir}/{self.tag}_betarelax_{self.seed}.pdb'
+        return pose_work, out_pdb, filter_scores
     def calc_hb(self,pose_work, verbose=False):
         print(f"[INFO] [RelaxScore] calc_hb for tag={self.tag}")
-        full_pose = packed_pose.to_pose(pose_work)
+        full_pose = pose_work
         #
         hbond_set = pyrosetta.rosetta.core.scoring.hbonds.HBondSet()
         full_pose.update_residue_neighbors()
@@ -130,27 +155,37 @@ def main():
     pdblist = sys.argv[1]
     ligname = sys.argv[2]
     lig_parm_fn = sys.argv[3] #ligand rosetta params file name
-    target_hbatm_names = sys.argv[4] #target ligand atom names to calculate hbond
+    target_hbatm_names = sys.argv[4] # kept for CLI compatibility; unused when HB calc is off
     dump_pdb = False
-    if len(sys.argv) > 5 and sys.argv[5] == 'dump_pdb':
-        dump_pdb = True
+    seed = 12345
+    if len(sys.argv) > 5:
+        if sys.argv[5] == 'dump_pdb':
+            dump_pdb = True
+            if len(sys.argv) > 6:
+                seed = int(sys.argv[6])
+        else:
+            seed = int(sys.argv[5])
     print("[INFO] ================================")
     print(f"[INFO] PDB list file : {pdblist}")
     print(f"[INFO] Ligand name   : {ligname}")
     print(f"[INFO] Params file   : {lig_parm_fn}")
     print(f"[INFO] Target HB atms: {target_hbatm_names}")
     print(f"[INFO] dump_pdb      : {dump_pdb}")
+    print(f"[INFO] seed          : {seed}")
     print("[INFO] ================================")
 
     #
     df_s = []
     with open(pdblist) as fp:
         for line in fp:
-            pyrosetta.init('-beta -gen_potential -in:file:native %s -extra_res_fa %s'%(line.strip(),lig_parm_fn))
+            pyrosetta.init(
+                '-beta -gen_potential -in:file:native %s -extra_res_fa %s -run:jran %d'
+                % (line.strip(), lig_parm_fn, seed)
+            )
             tmp_pdb = PocketPDB(line.strip(),ligname)
             repack_res = tmp_pdb.find_close_lig_contact_lig()
             #
-            pdb_score = RelaxScore(line.strip(),target_hbatm_names,repack_res=repack_res)
+            pdb_score = RelaxScore(line.strip(),target_hbatm_names,repack_res=repack_res,seed=seed)
             #
             csts = extract_dist_cst_from_pdb_use_allatm(line.strip(), ligname)
             fout = open(pdb_score.cst_fn,'w')
@@ -158,14 +193,11 @@ def main():
             fout.close()
             pose_init = pyrosetta.pose_from_pdb(line.strip())
             pose_work = pose_init.clone()
-            pose_work, out_pdb = pdb_score.bsite_repack_min(pose_work)
-            sc = pose_work.scores
-            # 수정
-            # hb_d = pdb_score.calc_hb(pose_work)
-            hb_d = pdb_score.calc_hb(pose_work, verbose=True)
-            sc.update(hb_d)
+            pose_work, out_pdb, filter_scores = pdb_score.bsite_repack_min(pose_work)
+            sc = {}
+            sc.update(filter_scores)
             if dump_pdb:
-                packed_pose.to_pose(pose_work).dump_pdb(f'{out_pdb}')
+                pose_work.dump_pdb(f'{out_pdb}')
             sc['tag'] = pdb_score.tag
             print(f"[DEBUG] Finished scoring tag={pdb_score.tag}")
             #
@@ -174,35 +206,6 @@ def main():
     sc_df = pd.DataFrame()
     if (len(df_s) > 0):
         sc_df = pd.concat(df_s,ignore_index=True)
-        # 수정
-        # hbatm_s = target_hbatm_names.split(',')
-        # hb_tags = []
-            # 1) 타겟 HB 원자 리스트
-        hbatm_s = [atm.strip() for atm in target_hbatm_names.split(",")]
-        print(f"[DEBUG] hbatm_s: {hbatm_s}")
-            # 2) lig_hb_sum 계산에 사용할 *_hbond 컬럼들
-        hb_sum_cols = [f"{atm}_hbond" for atm in hbatm_s]
-        print(f"[DEBUG] hb_sum_cols: {hb_sum_cols}")
-        sc_df['lig_hb_sum'] = sc_df[hb_sum_cols].sum(axis=1)
-
-            # 3) 출력 순서용: atom_hbond, atom_hbond_pairs 교대로
-        hb_cols_flat = []
-        for atm in hbatm_s:
-            hb_cols_flat.append(f"{atm}_hbond")
-            hb_cols_flat.append(f"{atm}_hbond_pairs")
-        print(f"[DEBUG] hb_cols_flat: {hb_cols_flat}")
-
-        base_cols = ['ddg', 'fa_rep', 'res_totalscore', 'totalscore']
-
-        # tag, lig_hb_sum 을 어디 둘지에 따라 순서 조정
-        fixed_cols = base_cols + hb_cols_flat
-        other_cols = [c for c in sc_df.columns if c not in fixed_cols]
-
-        print(f"[DEBUG] fixed_cols : {fixed_cols}")
-        print(f"[DEBUG] other_cols : {other_cols}")
-
-        sc_df = sc_df[fixed_cols + other_cols]
-
     else:
         print("[WARN] No scores collected (df_s is empty).")
 
